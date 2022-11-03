37721f6518a733d3ae8ef27521434bb020be0139
Sydney Harvey <sjh348@drexel.edu>
2022-11-02 22:12:42 -0400

    Beta Build Progress1

---
+++ beta/build/main.js
@@ -0,0 +1,40 @@
+const cart = document.querySelector("#cart");
+const pointer = document.querySelector("#pointer");
+const circle = document.querySelector("#circle");
+document.getElementById("element").addEventListener("mouseenter", cartEnter);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function cartEnter() {
+    console.log("Mouseover");
+    await sleep(500);
+    cart.classList.add("cartanim");
+    pointer.classList.add("pointeranim");
+    circle.classList.add("circleanim");
+    cart_items.classList.add("cart_itemsanim");
+    console.log("1");
+    await sleep(500);
+    cart.classList.remove("cartanim2");
+    pointer.classList.remove("pointeranim2");
+    circle.classList.remove("circleanim2");
+    cart_items.classList.remove("cart_itemsanim2");
+}
+
+document.getElementById("element").addEventListener("mouseleave", cartLeave);
+
+async function cartLeave() {
+    console.log("Mouseleave");
+    await sleep(500);
+    cart.classList.add("cartanim2");
+    pointer.classList.add("pointeranim2");
+    circle.classList.add("circleanim2");
+    cart_items.classList.add("cart_itemsanim2");
+    console.log("2");
+    await sleep(500);
+    cart.classList.remove("cartanim");
+    pointer.classList.remove("pointeranim");
+    circle.classList.remove("circleanim");
+    cart_items.classList.remove("cart_itemsanim");
+}
